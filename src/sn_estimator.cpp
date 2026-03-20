654195ac2224aefbd89878e3f3bb56f006e2066a
Dennis Alexis Valin Dittrich <davd@economicscience.net>
2026-03-20 23:40:10 +0100

    refactor: integrate worker_compat.h into qn/sn estimators (P0.1)
    Replace duplicate WorkerBase/SplitType definitions in qn_estimator.cpp
    and sn_estimator.cpp with a single #include "worker_compat.h". The
    redundant RcppParallel.h and TBB headers are also removed since
    worker_compat.h provides them under the same guards.

--- src/sn_estimator.cpp
+++ src/sn_estimator.cpp
@@ -8,25 +8,11 @@
 #include "qnsn_sort_utils.h"
 #include "qnsn_runtime_config.h"
 
-#include <RcppParallel.h>
-#ifdef USE_DIRECT_TBB
-// Include TBB headers
-#include <tbb/parallel_for.h>
-#include <tbb/parallel_reduce.h>
-#include <tbb/blocked_range.h>
-#endif
+#include "worker_compat.h"
 #include <algorithm>
 #include <memory>
 #include <type_traits>
 #include <cassert>
-
-#ifdef USE_DIRECT_TBB
-struct WorkerBase {};
-using SplitType = tbb::split;
-#else
-using WorkerBase = RcppParallel::Worker;
-using SplitType = RcppParallel::Split;
-#endif
 
 namespace robscale::qnsn {
 
